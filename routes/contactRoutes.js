300145a9fe240ed17485f8d9511110ba6bbf2b3c
dkachhot21 <dkachhot21@gmail.com>
2024-03-12 23:53:33 +0530

    Added Swagger

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,19 +1,204 @@
+//contactRoutes.js
 const express = require('express');
 const router = express.Router();
-const {constants} = require('../constants');
 const { getContacts, createContact, getContact, updateContact, deleteContact } = require('../controllers/contactController');
 const validateToken = require('../middleware/validateTokenHandler');
 
 router.use(validateToken); // Validate the token before any route is accessed
 
-router.route('/')
-    .get(getContacts)
-    .post(createContact);
+/**
+ * @swagger
+ * /api/contacts:
+ *  get:
+ *      tags:
+ *          - 2.) Authorization and CRUD
+ *      summary: "Returns all Contacts of the current user"
+ *      description: "returns all documents from database under the current user _id"
+ *      security:
+ *          - bearerAuth: []
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          200:
+ *              description: "All contacts"
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                      example:
+ *                          "name" : "contact1"
+ *                          "email": "contact1@root.com"
+ *                          "phone": "00000000"
+ *
+ */
+router.route("/").get(getContacts);
 
-router.route('/:id')
-    .get(getContact)
-    .patch(updateContact)
-    .delete(deleteContact);
+/**
+ * @swagger
+ * /api/contacts:
+ *  post:
+ *      tags:
+ *          - 2.) Authorization and CRUD
+ *      summary: "adds contact to database"
+ *      description: "adds document in mongodb based on current user _id"
+ *      security:
+ *          - bearerAuth: []
+ *      requestBody:
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          name:
+ *                              default: contact1
+ *                              type: string
+ *                          email:
+ *                              default: contact1@root.com
+ *                              type: string
+ *                          phone:
+ *                              default: 111111
+ *                              type: string
+ *              example:
+ *                  username: "contact1"
+ *                  email: "contact1@root.com"
+ *                  password: "111111"
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          200:
+ *              description: "created contact"
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                      example:
+ *                          "name": "ObjectId"
+ *                          "email": "contact@root.com"
+ *                          "phone": "111111"
+ *
+ */
+router.route("/").post(createContact);
 
+/**
+ * @swagger
+ * /api/contacts/{id}:
+ *  get:
+ *      tags:
+ *          - 2.) Authorization and CRUD
+ *      summary: "returns single contact"
+ *      description: "return single contact with respect to given id"
+ *      security:
+ *          - bearerAuth: []
+ *      parameters:
+ *          - in: path
+ *            Required: True
+ *            name: id
+ *            schema:
+ *              type: ObjectId
+ *            description: "numeric id of contact to get"
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          200:
+ *              description: "returned contact"
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                      example:
+ *                          "name": "contact12"
+ *                          "email": "contact12@root.com"
+ *                          "phone": "111111"
+ *
+ */
+router.route("/:id").get(getContact);
+
+/**
+ * @swagger
+ * /api/contacts/{id}:
+ *  put:
+ *      tags:
+ *          - 2.) Authorization and CRUD
+ *      summary: "updates single contact"
+ *      description: "updates single contact with respect to given id"
+ *      security:
+ *          - bearerAuth: []
+ *      parameters:
+ *          - in: path
+ *            Required: True
+ *            name: id
+ *            schema:
+ *              type: ObjectId
+ *            description: "numeric id of contact to update"
+ *      requestBody:
+ *          description: "content you want to change"
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          name:
+ *                              default: contact1
+ *                              type: string
+ *                          email:
+ *                              default: contact1@root.com
+ *                              type: string
+ *                          phone:
+ *                              default: 111111
+ *                              type: string
+ *              example:
+ *                  username: "contact1"
+ *                  email: "contact1@root.com"
+ *                  password: "111111"
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          200:
+ *              description: "updated contact"
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                      example:
+ *                          "name": "contact1_new"
+ *                          "email": "new_contact@root.com"
+ *                          "phone": "111111"
+ *
+ */
+router.route("/:id").put(updateContact)
+
+/**
+ * @swagger
+ * /api/contacts/{id}:
+ *  delete:
+ *      tags:
+ *          - 2.) Authorization and CRUD
+ *      summary: "deletes single contact"
+ *      description: "deletes single contact with respect to given id"
+ *      security:
+ *          - bearerAuth: []
+ *      parameters:
+ *          - in: path
+ *            Required: True
+ *            name: id
+ *            schema:
+ *              type: ObjectId
+ *            description: "numeric id of contact to delete"
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          200:
+ *              description: "Deleted contact"
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                      example:
+ *                          "name": "contact1"
+ *                          "email": "contact1@root.com"
+ *                          "phone": "111111"
+ *
+ */
+router.route("/:id").delete(deleteContact);
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -1,16 +1,59 @@
+//server.js
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const contactRoutes = require("./routes/contactRoutes");
-const userRoutes=require("./routes/userRoutes");
+const userRoutes = require("./routes/userRoutes");
 const errorHandler = require('./middleware/errorHandler');
-const {constants} = require('./constants');
 const connectDB = require('./config/dbConnection');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 const PORT = process.env.PORT || 3000;
 
-//Connect to Database
-connectDB();
+//Initialize Express app
 const app = express();
+
+//Swagger setup
+const swaggerOptions = {
+
+    swaggerDefinition: {
+        openapi: '3.0.3',
+        info: {
+            title: 'Backend Infrastructure of a Contact Manager Application',
+            description:'**1.) Authentication:**\n\n \ta.) User First Registers in the platform\n\n\tb.) Login will return an access token \n\n\tc.) Use token for authorization in the authorization button\n\n'+
+                        '**2.) Authorization and CRUD:**\n\n \ta.) Create some contacts under the existing user i.e {POST: /api/contacts} (one with the current token)\n\n\tb.)Use the other routes to perform the CRUD operations\n\n'+
+                        '*NOTE* : The token will expire every 30 min for demonstration purposes, the user needs to login again after 30 min ',
+            version: '0.0.1',
+        },
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    in: 'header',
+                    name: 'Authorization',
+                    description: 'Bearer Token',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
+        },
+        // security: [{
+        //     bearerAuth: [],
+        // },],
+        servers: [
+            {
+            url: 'http://localhost:3000/',
+            description: 'Local server',
+            },
+        ],
+    }, 
+    apis: ['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+
 
 app.get('/', (req, res) => {
     res.send(`API is running on port ${PORT}`);
@@ -20,12 +63,12 @@ app.get('/', (req, res) => {
 app.use(cors())
 app.use(express.json()); //Parsing the JSON data  from client side to server side
 
-
+//Connect to Database
+connectDB();
 
 //Routes
 app.use("/contact", contactRoutes);
-app.use("/user",userRoutes);
-
+app.use("/user", userRoutes);
 
 app.use(errorHandler);
 app.listen(PORT, () => {
